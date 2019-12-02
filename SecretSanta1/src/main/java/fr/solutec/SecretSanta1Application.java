d2bd2b4de16a196bfac6085a8f475e9f3e97c0d4
MartinMangeot <martin.mangeot.6@gmail.com>
2019-12-02 14:59:08 +0100

    hello

---
+++ SecretSanta1/src/main/java/fr/solutec/SecretSanta1Application.java
@@ -0,0 +1,14 @@
+package fr.solutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretSanta1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretSanta1Application.class, args);
+		System.out.println("hello");
+	}
+
+}
