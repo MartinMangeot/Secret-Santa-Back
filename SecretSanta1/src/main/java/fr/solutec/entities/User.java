8252ae6f8e7ec88de609954df49c69d782bb4311
Ysydra <bruna.seferian@gmail.com>
2019-12-02 15:51:23 +0100

    hello

--- SecretSanta1/src/main/java/fr/solutec/entities/User.java
+++ SecretSanta1/src/main/java/fr/solutec/entities/User.java
@@ -1,5 +1,70 @@
 package fr.solutec.entities;
 
 public class User {
+	private Long id;
+	private String mail;
+	private String mdp;
+	private String pseudo;
+	private Boolean inscriptionEnd;
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", mail=" + mail + ", mdp=" + mdp + ", pseudo=" + pseudo + ", inscriptionEnd="
+				+ inscriptionEnd + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public Boolean getInscriptionEnd() {
+		return inscriptionEnd;
+	}
+
+	public void setInscriptionEnd(Boolean inscriptionEnd) {
+		this.inscriptionEnd = inscriptionEnd;
+	}
+
+	public User() {
+		super();
+	}
+
+	public User(String mail, String mdp, String pseudo, Boolean inscriptionEnd) {
+		super();
+		this.mail = mail;
+		this.mdp = mdp;
+		this.pseudo = pseudo;
+		this.inscriptionEnd = inscriptionEnd;
+	}
+	
+	
 
 }
--- SecretSanta1/src/main/java/fr/solutec/rest/UserRest.java
+++ SecretSanta1/src/main/java/fr/solutec/rest/UserRest.java
@@ -1,5 +1,26 @@
 package fr.solutec.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.dao.UserRepository;
+import fr.solutec.entities.User;
+
+@RestController
+@CrossOrigin("*")
 public class UserRest {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public User inscriptionUser(@RequestBody User user) {
+
+		return userRepo.save(user);
+	}
 
 }
