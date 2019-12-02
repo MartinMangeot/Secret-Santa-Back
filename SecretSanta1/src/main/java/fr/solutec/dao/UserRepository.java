638bea802713304df67f7a2e2d35c2847a23242e
AubinM92 <aubin.mottier@gmail.com>
2019-12-02 16:55:03 +0100

    commit test

--- SecretSanta1/src/main/java/fr/solutec/dao/UserRepository.java
+++ SecretSanta1/src/main/java/fr/solutec/dao/UserRepository.java
@@ -1,9 +1,12 @@
 package fr.solutec.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+	
+	public Optional<User> findByMailAndMdp(String mail, String mdp);
 }
--- SecretSanta1/src/main/java/fr/solutec/rest/UserRest.java
+++ SecretSanta1/src/main/java/fr/solutec/rest/UserRest.java
@@ -1,5 +1,7 @@
 package fr.solutec.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +21,20 @@ public class UserRest {
 	
 	@RequestMapping(value = "/user", method = RequestMethod.POST)
 	public User inscriptionUser(@RequestBody User user) {
-
 		return userRepo.save(user);
+	}
+	
+	@RequestMapping(value = "/connexion", method = RequestMethod.POST)
+	public User connexion(@RequestBody User p) {
+		Optional<User> pe = userRepo.findByMailAndMdp(p.getMail(), p.getMdp());
+		User pf = new User();
+		if (pe.isPresent()) {	
+			
+			pf.setId(pe.get().getId());
+			pf.setMail(pe.get().getMail());
+			pf.setPseudo(pe.get().getPseudo());
+		}
+		return pf;
 	}
 
 }
