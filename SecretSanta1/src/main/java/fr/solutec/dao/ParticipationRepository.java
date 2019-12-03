e0e02ffb17e026c860642dc2cffa3a9577f5626d
tmpdevfr <thibault.mp@sfr.fr>
2019-12-03 13:03:33 +0100

    API participation

--- SecretSanta1/src/main/java/fr/solutec/dao/ParticipationRepository.java
+++ SecretSanta1/src/main/java/fr/solutec/dao/ParticipationRepository.java
@@ -1,10 +1,16 @@
 package fr.solutec.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Participation;
 
 public interface ParticipationRepository extends CrudRepository<Participation, Long>{
+	
+	public List<Participation> findByParticipantId(Long id);
+	
+	public List<Participation> findEvenementByParticipantId(Long id);
 
 
 
