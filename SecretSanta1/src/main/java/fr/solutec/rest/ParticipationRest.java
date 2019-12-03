f47a822854c6652a010e829e9e6868c5a6d6f774
Ysydra <bruna.seferian@gmail.com>
2019-12-03 17:06:14 +0100

    # WARNING: head commit changed in the meantime
    user

--- SecretSanta1/src/main/java/fr/solutec/rest/ParticipationRest.java
+++ SecretSanta1/src/main/java/fr/solutec/rest/ParticipationRest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.solutec.dao.ParticipationRepository;
 import fr.solutec.entities.Participation;
 import fr.solutec.entities.SSanta;
+import fr.solutec.entities.User;
 
 @RestController
 @CrossOrigin("*")
@@ -45,6 +47,11 @@ public class ParticipationRest {
 	public List<Participation> recupParticipants(@PathVariable Long id) {
 		return participationRepo.findParticipantByEvenementId(id);
 	}
+	
+	//@RequestMapping(value = "/home/invitation", method = RequestMethod.POST)
+	//public Participation creerInvitation(@PathVariable Long id) {
+		
+//	}
 
 
 	
