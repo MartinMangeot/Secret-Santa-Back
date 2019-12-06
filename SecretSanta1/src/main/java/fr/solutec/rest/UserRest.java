ee4fcb0686f63a4513dfea461a6f67a1453c0771
Ysydra <bruna.seferian@gmail.com>
2019-12-06 15:57:40 +0100

    mail

--- SecretSanta1/src/main/java/fr/solutec/rest/UserRest.java
+++ SecretSanta1/src/main/java/fr/solutec/rest/UserRest.java
@@ -2,6 +2,15 @@ package fr.solutec.rest;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -46,10 +55,44 @@ public class UserRest {
 			
 			pf.setMail(pe.get().getMail());
 			pf.setInscriptionEnd(false);
+			sendMail(pf.getMail());
 			userRepo.save(pf);
 			return pf;	
 			}						
 		}
+	
+	private void sendMail(String mail) {
+		Properties props = new Properties();
+		final String expediteur = "secret.santa.dev17@gmail.com";
+		final String password = "dev17test!";
+		final String objet = "Bienvenue sur Secret Santa !";
+		final String contenu = "Bonjour, vous avez été invité à participer à un événement secret santa !! Pour rejoindre vos amis et faire de nombreux heureux, inscrivez vous sur http://localhost:4200/ !!";
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(expediteur,password);
+			}
+		});
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(expediteur));
+			message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(mail));
+			message.setSubject(objet);
+			message.setText(contenu);
+
+			Transport.send(message);
+
+			System.out.println("Mail envoyé");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 	
 	
 	@RequestMapping(value = "/user", method = RequestMethod.POST)
